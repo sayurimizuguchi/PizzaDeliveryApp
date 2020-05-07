9dbefb23aba089ce31bc44b25615627205aa15d4
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-05-07 17:38:54 -0400

    initial commit

---
+++ src/components/card-box.component.js
@@ -0,0 +1,33 @@
+// @flow
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+type Props = {
+  children: *,
+  image: number,
+};
+
+export const CardBox = ({ children, image }: Props) => (
+  <View style={styles.whiteCard}>
+    <Image source={image} style={styles.img} />
+    <View style={styles.view}>{children}</View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  whiteCard: {
+    backgroundColor: Colors.white,
+    borderRadius: 1,
+    marginBottom: 28,
+    marginTop: 28,
+  },
+  img: {
+    resizeMode: 'contain',
+  },
+  view: {
+    flex: 1,
+    marginLeft: 32,
+    alignSelf: 'center',
+  },
+});
