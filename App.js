4e668120d48064a6c2a93a8174589a98980a19d2
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-05-12 12:14:01 -0400

    WIP app update

--- App.js
+++ App.js
@@ -8,19 +8,45 @@ import {
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-import { paymentListResponse } from './src/general.constants';
+import { getPaymentList } from './src/api/request';
 import { CardBox } from './src/components/card-box.component';
 import { TextDefault } from './src/components/text.component';
+import { GET_PAYMENT_LIST } from './src/general.constants';
+import { transformRespone } from './src/api/request.utils';
+
 
 export default class App extends Component {
   constructor(properties) {
     super(properties);
     this.state = {
-      orders: paymentListResponse,
+      orders: {},
     };
     this.metadata = {};
+    this.defaultImage =
+      'https://cdn.icon-icons.com/icons2/1465/PNG/512/543pizza1_100912.png';
   }
 
+  async componentWillMount () {
+    console.log(' BEFORE this.state.orders', this.state.orders)
+    try { 
+      await fetch(GET_PAYMENT_LIST, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => transformRespone(response))
+      .then(responseJSON => {
+        this.setState({ orders: responseJSON })
+     });
+  
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(' AFTER this.state.orders', this.state.orders)
+  }
+  
   render() {
     return (
       <>
@@ -30,11 +56,11 @@ export default class App extends Component {
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
             <View style={styles.box}>
-              {this.state.orders.paymentIntents.data.map(
+              {/* {this.state.orders.paymentIntents.data.map(
                 ({ metadata }, index) => {
-                  console.log('name', metadata.delivery.image);
+                  console.log('name', metadata);
                   return (
-                    <CardBox source={metadata.delivery.image} key={index}>
+                    <CardBox source={this.defaultImage} key={index}>
                       <TextDefault>
                         {metadata.delivery.addressDelivery}
                       </TextDefault>
@@ -45,7 +71,7 @@ export default class App extends Component {
                     </CardBox>
                   );
                 },
-              )}
+              )} */}
             </View>
             <View style={styles.body} />
           </ScrollView>
