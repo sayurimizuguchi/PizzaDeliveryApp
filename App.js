33742f232d5435926b693c0928e512bfe137f764
Sayuri Mizuguchi <adoravel_sansei@hotmail.com>
2020-05-12 12:17:17 -0400

    wip

--- App.js
+++ App.js
@@ -46,7 +46,30 @@ export default class App extends Component {
     }
     console.log(' AFTER this.state.orders', this.state.orders)
   }
+
+  async completePayment () {
+    try { 
+      await fetch(CONFIRM_PAYMENT, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: {
+          id: this.state.orders.data,
+        }
+      })
+      .then(response => transformRespone(response))
+      .then(responseJSON => {
+        this.setState({ orders: responseJSON })
+     });
   
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(' AFTER this.state.orders', this.state.orders)
+  }
+
   render() {
     return (
       <>
